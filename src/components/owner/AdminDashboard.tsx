51fb1ed92c697f0c283a702bfc8e7b65b47f1f06
unknown <naodg@gmail.com>
2025-05-14 10:39:08 +0900

    냥

--- src/components/owner/AdminDashboard.tsx
+++ src/components/owner/AdminDashboard.tsx
@@ -41,13 +41,35 @@ export default function AdminDashboard() {
   if (userStoreId !== storeId) return <p>해당 가게에 대한 접근 권한이 없습니다.</p>
 
   return (
-    <div className="regular-wrapper">
-      <h2>{storeName}의 단골 목록</h2>
-      <ul>
-        {favorites.map((f, i) => (
-          <li key={i}>{f.nickname} | {f.phone}</li>
-        ))}
-      </ul>
+    <div className="admin-wrapper">
+      <h2 className="admin-title">
+        <img src="/SAMGA-V2//img/icon/수퍼히어로랜딩.svg" alt="캐릭터" className="admin-icon" />
+        {storeName} 단골 리스트
+      </h2>
+
+      <table className="admin-table">
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>전화번호</th>
+            <th>이메일</th>
+            <th>
+              <img src="/SAMGA-V2//img/icon/download.svg" alt="다운로드" className="download-icon" />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {favorites.map((f, i) => (
+            <tr key={i}>
+              <td>{f.nickname}</td>
+              <td>{f.phone}</td>
+              <td>{f.email}</td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
+
   )
 }
