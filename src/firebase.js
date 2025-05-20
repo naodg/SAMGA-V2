caca088f197ddb5d201048b052827c0d8003ad3d
unknown <naodg@gmail.com>
2025-05-20 10:44:20 +0900

    냥

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,19 @@
 // src/firebase.ts
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
     apiKey: "AIzaSyD14pfYMFzHtSZpo4K6j-uzv9H9SbqMbLg",
     authDomain: "samgabeef.firebaseapp.com",
     projectId: "samgabeef",
-    storageBucket: "samgabeef.firebasestorage.app",
+    storageBucket: "samgabeef.appspot.com",
     messagingSenderId: "211043237282",
     appId: "1:211043237282:web:50294933424dcba04e86fb",
-    measurementId: "G-JNZJXH570Z"
+    measurementId: "G-JNZJXH57QZ"
 };
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+// ✅ 중복 초기화 방지
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
